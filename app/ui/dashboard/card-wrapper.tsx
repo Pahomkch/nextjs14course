5f2dc529e7a1e9a89c8ecad3b366ee3d9c3002f0
Andrew Pahomov <pahomov.kch@gmail.com>
2023-11-12 00:29:59 +0800

    chapter 9 - streaming
    Straming can apply for 1) page with loading.ts 2) Component with <Suspence>

---
+++ app/ui/dashboard/card-wrapper.tsx
@@ -0,0 +1,15 @@
+import {Card} from "./cards"
+import {fetchCardData} from "@/app/lib/data"
+
+export const CardWrapper = async (props: {className?: string}) => {
+  const {totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers} = await fetchCardData()
+
+  return (
+    <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+    </div>
+  )
+}
