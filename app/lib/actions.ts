f7ec5330eb21e8f128308d606529b4fe65538a11
Andrew Pahomov <pahomov.kch@gmail.com>
2023-11-13 13:38:26 +0800

    chapter-13. Error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,8 +17,14 @@ const CreateInvoice = InvoiceSchema.omit({id: true, date: true})
 const UpdateInvoice = InvoiceSchema.omit({id: true, date: true})
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  throw new Error("Failed to Delete Invoice")
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`
+    revalidatePath("/dashboard/invoices")
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export async function createInvoice(formData: FormData) {
@@ -31,16 +37,20 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100
   const date = new Date().toISOString().split("T")[0]
 
-  await sql`
+  try {
+    await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
 `
+  } catch (error) {
+    console.error(error)
+  }
+
   revalidatePath("/dashboard/invoices")
   redirect("/dashboard/invoices")
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  console.log("Edit action")
   const {customerId, amount, status} = UpdateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -49,11 +59,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100
 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `
+  } catch (error) {
+    console.error(error)
+  }
 
   revalidatePath("/dashboard/invoices")
   redirect("/dashboard/invoices")
